9acad30f186131b035901a0fe60bfa7337fd7044
zeyakarm12 <zeya.karim@depo24.in>
2024-11-10 14:03:15 +0530

    [Feat] Attendance Data Fetched From Db

--- app/(dashboards)/attendance/page.js
+++ app/(dashboards)/attendance/page.js
@@ -1,14 +1,58 @@
-import { attendanceColumns } from "@/components/Columns";
 import TableComponent from "@/components/Table/Table";
-import { attendance } from "@/lib/data";
+
+const columns = [
+    {
+        field: 'student',
+        headerName: 'Student Name',
+        headerClassName: 'super-app-theme--header',
+        flex: 1.7,
+    },
+    {
+        field: 'date',
+        headerName: 'Date',
+        headerClassName: 'super-app-theme--header',
+        flex: 1,
+    },
+    {
+        field: 'lesson',
+        headerName: 'Lesson',
+        headerClassName: 'super-app-theme--header',
+        flex: 1
+    },
+    {
+        field: 'class',
+        headerName: 'Class',
+        headerClassName: 'super-app-theme--header',
+        flex: 0.7,
+    },
+    {
+        field: 'present',
+        headerName: 'Attendance',
+        headerClassName: 'super-app-theme--header',
+        flex: 0.7,
+    },
+    // {
+    //     field: 'created_at',
+    //     headerName: 'Created At',
+    //     headerClassName: 'super-app-theme--header',
+    //     flex: 1.7
+    // },
+];
 
 const Attendance = () => {
     return (
         <div className='w-[98%] mx-auto'>
             <TableComponent 
                 title={'All Attendance'}
-                columns={attendanceColumns}
-                data={attendance}
+                columns={columns}
+                rowId={'id'}
+                endPoint={'/attendances'}
+                dataPosition={'attendances'}
+                checkBoxSelection={false}
+                version='version-1'
+                dialogTitle='Create A New Attendance'
+                table="attendance"
+                type="create"
             />
         </div>
     )
--- app/api/attendances/route.js
+++ app/api/attendances/route.js
@@ -14,11 +14,42 @@ export async function POST(req) {
     }
 }
 
+const simplifiedAttendances = (attendances) => {
+    return attendances?.map((attendance) => {
+        const lastName = attendance?.student?.last_name ? attendance?.student?.last_name : '';
+        const simplifiedAttendance = {
+            ...attendance,
+            student: attendance?.student?.first_name + ' ' + lastName,
+            class: attendance?.class?.name,
+            lesson: attendance?.lesson?.name
+        }
+        return simplifiedAttendance
+    })
+}
 
 export async function GET(req) {
     try {
-        const attendances = await prisma.attendance.findMany();
-        return NextResponse.json({data: {attendances}, status: 200});
+        const attendances = await prisma.attendance.findMany({
+            include: {
+                student: {
+                    select: {
+                        first_name: true,
+                        last_name: true
+                    }
+                },
+                class: {
+                    select: {
+                        name: true
+                    }
+                },
+                lesson: {
+                    select: {
+                        name: true
+                    }
+                }
+            }
+        });
+        return NextResponse.json({data: {attendances: simplifiedAttendances(attendances), status: 200, maxPage: 1, page: 1}});
     } catch (error) {
         console.log("Error:",error)
         return NextResponse.json({"msg": "something went wrong"},  {status:'400'})
