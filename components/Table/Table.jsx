4e87437494cdb0bb5d030144ad0ac55e1ccb150a
zeyakarm12 <zeya.karim@depo24.in>
2025-02-19 23:39:58 +0530

    [Feat] Mobile Responsive File Update

--- components/Table/Table.jsx
+++ components/Table/Table.jsx
@@ -1,6 +1,6 @@
 import DesktopResponsiveTable from "./DesktopResponsiveTable";
-import MobileResponsiveTable from "./MobileResponsiveTable";
 import { useEffect, useState } from "react";
+import MobileResponsiveTable from "./MobileResponsiveTable";
 
 export default function Table(props) {
     const [isMobile, setIsMobile] = useState(false); // Avoid SSR issue by setting a safe default
