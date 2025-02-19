50517d85b90f92e2c7336b74926dfb3f0327fd2a
zeyakarm12 <zeya.karim@depo24.in>
2025-02-19 13:41:41 +0530

    [update] file name update next.config.mjs to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
     images: {
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "school-management-v1.s3.ap-south-1.amazonaws.com",
-                pathname: "/Icons/**", // Allow all images from the "Icons" folder
+                pathname: "/Icons/*",
             },
             {
                 protocol: "https",
                 hostname: "school-management-v1.s3.ap-south-1.amazonaws.com",
-                pathname: "/students/**", // Allow all images from the "students" folder
+                pathname: "/students/*",
             },
             {
                 protocol: "https",
                 hostname: "school-management-v1.s3.ap-south-1.amazonaws.com",
-                pathname: "/teachers/**", // Allow all images from the "teachers" folder
+                pathname: "/teachers/*",
             },
         ],
         unoptimized: true,
     },
 };
-
-export default nextConfig;
