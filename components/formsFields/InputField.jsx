3ce2ce127c1bd2a2dbf068d220cec8df770fa8fb
zeyakarm12 <zeya.karim@depo24.in>
2025-03-05 12:18:04 +0530

    [Feat] When User Create and Update Phone number min and max limit

--- components/formsFields/InputField.jsx
+++ components/formsFields/InputField.jsx
@@ -4,18 +4,24 @@ const InputField = ({ type, label, name, icon, isRequired, className, value, onC
     const handleChange = (e) => {
         let newValue = e?.target?.value;
 
-        if (type === "number") {
-            newValue = newValue.replace(/\D/g, ""); 
-
+        if (
+            type === "number" && 
+            !["capacity", "percentage", "mark", "total"].includes(name) // More readable condition
+        ) {
+            // Remove non-numeric characters
+            newValue = newValue.replace(/\D/g, "");
+        
+            // Limit phone numbers to 10 digits
             if (name === "phone") {
                 newValue = newValue.slice(0, 10);
             }
-
+        
+            // Convert to a number and pass to onChange
             let numericValue = Number(newValue);
             onChange(name, numericValue);
         } else {
             onChange(name, newValue);
-        }
+        }        
     };
 
     const handleBlur = () => {
