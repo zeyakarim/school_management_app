3fb4cff7b691a35ae3ee6d9f7cf7fc0c6ad4af06
zeyakarm12 <zeya.karim@depo24.in>
2025-03-05 12:13:43 +0530

    [Feat] When User Create and Update Phone number min and max limit

--- components/formsFields/InputField.jsx
+++ components/formsFields/InputField.jsx
@@ -1,22 +1,45 @@
 import { Input } from "@nextui-org/react";
 
-const InputField = ({type, label, name, icon, isRequired, className, value, onChange }) => {
+const InputField = ({ type, label, name, icon, isRequired, className, value, onChange, min, max, step }) => {
+    const handleChange = (e) => {
+        let newValue = e?.target?.value;
+
+        if (type === "number") {
+            newValue = newValue.replace(/\D/g, ""); 
+
+            if (name === "phone") {
+                newValue = newValue.slice(0, 10);
+            }
+
+            let numericValue = Number(newValue);
+            onChange(name, numericValue);
+        } else {
+            onChange(name, newValue);
+        }
+    };
+
+    const handleBlur = () => {
+        if (name === "phone" && value.toString().length !== 10) {
+            alert("Phone number must be exactly 10 digits.");
+        }
+    };
+
     return (
         <Input
             required={isRequired}
-            isRequired={isRequired}
             type={type}
             label={label}
             name={name}
             value={value}
-            onChange={(e) => onChange(name, e?.target?.value)}  // Pass both name and date
+            onChange={handleChange}
+            onBlur={handleBlur}
             endContent={icon}
             variant="bordered"
             className={className}
             labelPlacement="outside"
             autoComplete="off"
         />
-    )
-}
+    );
+};
 
 export default InputField;
