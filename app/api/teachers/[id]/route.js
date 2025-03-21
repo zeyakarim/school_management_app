b6dffb4dcfb586d98f0dbe362c480c8869caae36
zeyakarm12 <zeya.karim@depo24.in>
2025-03-21 23:26:07 +0530

    [Feat] Data update

--- app/api/teachers/[id]/route.js
+++ app/api/teachers/[id]/route.js
@@ -10,9 +10,9 @@ const bucketName = process.env.AWS_S3_BUCKET;
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+    region: process.env.AWS_S3_REGION
 });
 
 const s3 = new AWS.S3();
