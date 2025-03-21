51531dace18923b77809ea70b506132b9737603a
zeyakarm12 <zeya.karim@depo24.in>
2025-03-21 23:18:59 +0530

    [Fix] Aws Setup

--- app/api/teachers/[id]/route.js
+++ app/api/teachers/[id]/route.js
@@ -7,23 +7,48 @@ import { readDocumentsFromS3 } from "@/utils/s3";
 
 const bucketName = process.env.AWS_S3_BUCKET;
 
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
+
+const s3 = new AWS.S3();
+
+// export async function GET(req, res) {
+//     try {
+//         const result = await s3.listBuckets().promise();
+//         console.log("S3 Buckets:", result);
+//         return res.json({ success: true, buckets: result.Buckets });
+//     } catch (error) {
+//         console.error("S3 Access Error:", error);
+//         return res.status(500).json({ success: false, error: error.message });
+//     }
+// }
+
+
 export async function GET(req, { params }) {
     try {
-        const teacherDetails = await prisma.teacher.findUnique({
-            where: {id: parseInt(params?.id)}
-        });
-        // const teacherDetailsItems = await fetchIcons();
+        // const teacherDetails = await prisma.teacher.findUnique({
+        //     where: {id: parseInt(params?.id)}
+        // });
+        // // const teacherDetailsItems = await fetchIcons();
 
-        const attachDocsUrl = await readDocumentsFromS3('teachers', teacherDetails?.id, bucketName);
-        console.log(attachDocsUrl,'attachDocsUrl')
-        if (attachDocsUrl) teacherDetails['img'] = attachDocsUrl?.[0] || null;
+        // const attachDocsUrl = await readDocumentsFromS3('teachers', teacherDetails?.id, bucketName);
+        // console.log(attachDocsUrl,'attachDocsUrl')
+        // if (attachDocsUrl) teacherDetails['img'] = attachDocsUrl?.[0] || null;
 
-        const data = {
-            ...teacherDetails,
-            // detailsItems: studentDetailsItems
-        }
+        // const data = {
+        //     ...teacherDetails,
+        //     // detailsItems: studentDetailsItems
+        // }
 
-        return NextResponse.json(success(data, 'Teacher Details Fetched Successfully'));
+        // return NextResponse.json(success(data, 'Teacher Details Fetched Successfully'));
+        const result = await s3.listBuckets().promise();
+        console.log("S3 Buckets:", result);
+        return res.json({ success: true, buckets: result.Buckets });
     } catch (error) {
         console.error("Error fetching teacher:", error);
         return NextResponse.json(failure(error, error?.message));
